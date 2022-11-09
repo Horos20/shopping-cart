a77d6d7ad09fb3485ce0059f33c31249b099cd86
Tanel Märjama <tanel.marjama@khk.ee>
2022-11-09 23:22:58 +0200

    android react-navigation conf

--- android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -1,18 +1,12 @@
-import android.os.Bundle;
-
-
 package com.awesomeproject;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
+
 
 public class MainActivity extends ReactActivity {
-
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-  super.onCreate(null);
-}
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -21,6 +15,11 @@ protected void onCreate(Bundle savedInstanceState) {
   @Override
   protected String getMainComponentName() {
     return "AwesomeProject";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
